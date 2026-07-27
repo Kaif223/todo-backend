eb804718bce6c42e1f24ea18708a1606109cfbbc
Kaif <kaif@example.com>
2026-07-27 14:52:56 +0500

    add middleaware in routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -8,10 +8,10 @@ const verifyToken = require("../middleaware/verifyToken");
 
 router.route('/api/userDetail').get(verifyToken, userController.userGet)
 router.route("/api/signup").post(userController.userPost)
-router.route("/api/userDetail/:id").delete(userController.userDelete)
-router.route("/api/userDetail/:id").patch(userController.userPatch)
+router.route("/api/userDetail/:id").delete(verifyToken, userController.userDelete)
+router.route("/api/userDetail/:id").patch(verifyToken, userController.userPatch)
 router.route("/api/signin").post(userController.userSignin)
-router.route("/api/logout").post(userController.userSignout)
+router.route("/api/logout").post(verifyToken, userController.userSignout)
 
 
 module.exports = router;
