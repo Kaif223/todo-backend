b16fa63e431d880a07cfe997a3db9e71b121cdb5
Zulqaif <zulkaif044@gmail.com>
2026-07-29 11:28:50 +0500

    feat: add token verification middleware to todo routes

--- src/routes/todo.route.js
+++ src/routes/todo.route.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 // const {todoGet, todoPost} = require('../controllers/todo.controller')
-const {todoscontroller} = require('../controllers')
+const {todoscontroller} = require('../controllers');
+const verifyToken = require("../middleaware/verifyToken");
 
 
 
-router.route('/api/todos').get(todoscontroller.todoGet)
-router.route("/api/todos").post(todoscontroller.todoPost)
-router.route("/api/todos/:id").delete(todoscontroller.todoDelete)
-router.route("/api/todos/:id").patch(todoscontroller.todoPatch)
+router.route('/api/todos').get(verifyToken, todoscontroller.todoGet)
+router.route("/api/todos").post(verifyToken, todoscontroller.todoPost)
+router.route("/api/todos/:id").delete(verifyToken, todoscontroller.todoDelete)
+router.route("/api/todos/:id").patch(verifyToken, todoscontroller.todoPatch)
 
 module.exports = router;
